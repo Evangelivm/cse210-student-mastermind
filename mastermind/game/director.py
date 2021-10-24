6ca6c29a43046aeb8a4e8269fa9fd31fb8feb9bb
Evangelivm <devysalomon@gmail.com>
2021-10-24 12:48:30 -0500

    changes

--- mastermind/game/director.py
+++ mastermind/game/director.py
@@ -3,6 +3,7 @@ from game.console import Console
 from game.move import Move
 from game.player import Player
 from game.roster import Roster
+import time
 
 class Director:
     """A code template for a person who directs the game. The responsibility of 
@@ -37,6 +38,7 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
+        self._console.intro()
         self._prepare_game()
         self._board.random()
         while self._keep_playing:
@@ -51,6 +53,7 @@ class Director:
             self (Director): An instance of Director.
         """
         for n in range(2):
+            time.sleep(0.5)
             name = self._console.read(f"Enter a name for player {n + 1}: ")
             self._board.players(name)
             player = Player(name)
@@ -70,6 +73,7 @@ class Director:
         # get next player's move
         player = self._roster.get_current()
         self._console.write(f"{player.get_name()}'s turn:")
+        time.sleep(0.5)
         pile = self._console.read_number("What is your guess? ")
         player.set_move(pile)
 
@@ -96,6 +100,8 @@ class Director:
             winner = self._roster.get_current()
             name = winner.get_name()
             print(f"\n{name} won!")
+            time.sleep(0.5)
+            self._console.winner()
             self._keep_playing = False
         else:
             self._roster.next_player()
